d61829892383b08a20c663cadcfc691d36e9ee3d
Chaotixy <36245713+Chaotixy@users.noreply.github.com>
2018-10-17 21:39:04 +0200

    Beginning and video works

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="CSS/style.css">
+        <title>Battlebots</title>
+    </head>
+    <body>
+         <?php
+        // put your code here
+        ?>
+        
+        <div id ="wrapper">
+            <div id="header">
+                <div id="headMiddle">
+                    <div id="headtext"><h1>Battle-Bots</h1></div>
+                    <div id="headlogo"><img src=img/controller.png alt="controller" height="70px" width="70px"></div>
+                </div>
+                
+                <div class="half">
+                    <iframe src="http://foscam.serverict.nl/videostream.cgi"></iframe>
+                </div>
+                <div class="half">
+                    
+                </div>
+            </div>
+            
+        </div>
+       
+    </body>
+</html>
