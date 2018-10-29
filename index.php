e0244c21732be06cb49d9e58b7010a4ac163774a
Amyra <danique.roke@student.stenden.com>
2018-10-29 13:36:32 +0100

    refresh fixed

--- index.php
+++ index.php
@@ -60,33 +60,11 @@
                                         <script type="text/javascript">
                                             $(document).ready(function () {
                                                 setInterval(function () {
-                                                    $('#scoreboard').load('index.php');
+                                                    $('#scoreboard').load('table.php');
                                                 }, 1000);
                                             });
                                         </script>
                          
-                                        <?php
-                                        $conn = mysqli_connect("localhost", "root", "", "battlebot");
-                                        if ($conn->connect_error) {
-                                            die("Connection failed:" . $conn->connect_error);
-                                        }
-
-                                        $sql = "SELECT quantity1, quantity2, quantity3, quantity4, quantity5, quantity6, "
-                                                . "quantity7, quantity8, quantity9, quantity10, quantity11, quantity12 from board ";
-                                        $result = $conn->query($sql);
-
-                                        if ($result->num_rows > 0) {
-                                            while ($row = $result->fetch_assoc()) {
-                                                echo "<tr><th>" . "Battlebot" . "</th><th>" . "Game1" . "</th><th>" . "Game2" . "</th><th>" . "Game3" . "</th></tr>";
-                                                echo "<tr><td>" . "Group 1" . "</td><td>" . $row["quantity1"] . "</td><td>" . $row["quantity2"] . "</td><td>" . $row["quantity3"] . "</td></tr>";
-                                                echo "<tr><td>" . "Group 2" . "</td><td>" . $row["quantity4"] . "</td><td>" . $row["quantity5"] . "</td><td>" . $row["quantity6"] . "</td></tr>";
-                                                echo "<tr><td>" . "Group 3" . "</td><td>" . $row["quantity7"] . "</td><td>" . $row["quantity8"] . "</td><td>" . $row["quantity9"] . "</td></tr>";
-                                                echo "<tr><td>" . "Group 4" . "</td><td>" . $row["quantity10"] . "</td><td>" . $row["quantity11"] . "</td><td>" . $row["quantity12"] . "</td></tr>";
-                                            }
-                                        }
-                                        $conn->close();
-                                        
-                                        ?>
 
 
                                     </table>
