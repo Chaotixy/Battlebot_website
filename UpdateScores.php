bb2fc9c796fce4077c6676915aac76ed6d38d261
Romandy <romandy.richardson@student.stenden.com>
2018-10-30 13:52:02 +0100

    Revert "new images, addition of new css files, made changes some adjustments to the old ones"
    This reverts commit 404a1ffa0808ed0e7634c9f99e9343163879d42f.

--- UpdateScores.php
+++ UpdateScores.php
@@ -1,27 +1,22 @@
-<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="CSS/style.css">
-        <link rel="stylesheet" type="text/css" href="CSS/responsive.css">
-        <link rel="stylesheet" type="text/css" href="CSS/main.css">
-        <script src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <title>Battlebots</title>
     </head>
     <body>
 
         <?php
-        session_start();
+
+            session_start();
         if (!isset($_SESSION['userName']) || empty($_SESSION['userName']))
         {
             header("location: login.php");
             exit;
         }
-
+        
         include_once "connect.php";
+
         ?>
 
         <div id ="wrapper">
@@ -65,8 +60,7 @@
                     </div>
                     <div id="LiveInfo">
                         <div id="video">
-                            <iframe id="dome" src="http://foscam.serverict.nl/videostream.cgi"></iframe>
-                            <iframe id="dome-control" frameborder="0" hidden></iframe>
+                            <iframe src="http://foscam.serverict.nl/videostream.cgi"></iframe>
                         </div>
                         <div id="introTextBox">
                             <h1>Scoreboard</h1>
@@ -74,8 +68,7 @@
                             <div id="table">
                                 <table id="scoreboard">
                                     <?php
-                                    if (isset($_POST['submit']))
-                                    {
+                                    if (isset($_POST['submit'])) {
                                         $quantity1 = $_POST['quantity1'];
                                         $quantity2 = $_POST['quantity2'];
                                         $quantity3 = $_POST['quantity3'];
@@ -92,15 +85,12 @@
                                         $query = "UPDATE `board` SET `ID`=1,`quantity1`=?,`quantity2`=?,`quantity3`=?,`quantity4`=?,`quantity5`=?,`quantity6`=?,`quantity7`=?,`quantity8`=?,`quantity9`=?,`quantity10`=?,`quantity11`=?,`quantity12`=? WHERE 1";
 
 
-                                        if ($stmt = mysqli_prepare($conn, $query))
-                                        {
+                                        if ($stmt = mysqli_prepare($conn, $query)) {
                                             mysqli_stmt_bind_param($stmt, 'iiiiiiiiiiii', $quantity1, $quantity2, $quantity3, $quantity4, $quantity5, $quantity6, $quantity7, $quantity8, $quantity9, $quantity10, $quantity11, $quantity12);
                                             $queryResult = mysqli_stmt_execute($stmt);
-                                            if ($queryResult === FALSE)
-                                            {
+                                            if ($queryResult === FALSE) {
                                                 echo "<p>Unable to execute the query.</p>" . "Error code" . mysqli_erno($conn);
-                                            } else
-                                            {
+                                            } else {
                                                 echo "";
                                             }
 
@@ -123,69 +113,48 @@
                                                         . "quantity7, quantity8, quantity9, quantity10, quantity11, quantity12 from board ";
                                                 $result = $conn->query($sql);
 
-                                                if ($result->num_rows > 0)
-                                                {
+                                                if ($result->num_rows > 0){
+                                                    
+                                                while ($row = $result->fetch_assoc()){
+                                                
+                                                    
+                                                    ?>
 
-                                                    while ($row = $result->fetch_assoc())
-                                                    {
-                                                        ?>
+                                                <tr>
+                                                    <td>Group 1</td>
+                                                    <td><input type="number" name="quantity1" min="0" max="10"  class="scores" value="<?php echo $row["quantity1"]?>"></td>
+                                                    <td><input type="number" name="quantity2" min="0" max="10" class="scores" value="<?php echo $row["quantity2"]?>"></td>
+                                                    <td><input type="number" name="quantity3" min="0" max="10" class="scores" value="<?php echo $row["quantity3"]?>"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Group 2</td>
+                                                    <td><input type="number" name="quantity4" min="0" max="10" class="scores" value="<?php echo $row["quantity4"]?>"></td>
+                                                    <td><input type="number" name="quantity5" min="0" max="10" class="scores" value="<?php echo $row["quantity5"]?>"></td>
+                                                    <td><input type="number" name="quantity6" min="0" max="10" class="scores" value="<?php echo $row["quantity6"]?>"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Group 3</td>
+                                                    <td><input type="number" name="quantity7" min="0" max="10" class="scores" value="<?php echo $row["quantity7"]?>"></td>
+                                                    <td><input type="number" name="quantity8" min="0" max="10" class="scores" value="<?php echo $row["quantity8"]?>"></td>
+                                                    <td><input type="number" name="quantity9" min="0" max="10" class="scores" value="<?php echo $row["quantity9"]?>"></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Group 4</td>
+                                                    <td><input type="number" name="quantity10" min="0" max="10" class="scores" value="<?php echo $row["quantity10"]?>"></td>
+                                                    <td><input type="number" name="quantity11" min="0" max="10" class="scores" value="<?php echo $row["quantity11"]?>"></td>
+                                                    <td><input type="number" name="quantity12" min="0" max="10" class="scores" value="<?php echo $row["quantity12"]?>"></td>
+                                                </tr>
 
-                                                        <tr>
-                                                            <td>Group 1</td>
-                                                            <td><input type="number" name="quantity1" min="0" max="10"  class="scores" value="<?php echo $row["quantity1"] ?>"></td>
-                                                            <td><input type="number" name="quantity2" min="0" max="10" class="scores" value="<?php echo $row["quantity2"] ?>"></td>
-                                                            <td><input type="number" name="quantity3" min="0" max="10" class="scores" value="<?php echo $row["quantity3"] ?>"></td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Group 2</td>
-                                                            <td><input type="number" name="quantity4" min="0" max="10" class="scores" value="<?php echo $row["quantity4"] ?>"></td>
-                                                            <td><input type="number" name="quantity5" min="0" max="10" class="scores" value="<?php echo $row["quantity5"] ?>"></td>
-                                                            <td><input type="number" name="quantity6" min="0" max="10" class="scores" value="<?php echo $row["quantity6"] ?>"></td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Group 3</td>
-                                                            <td><input type="number" name="quantity7" min="0" max="10" class="scores" value="<?php echo $row["quantity7"] ?>"></td>
-                                                            <td><input type="number" name="quantity8" min="0" max="10" class="scores" value="<?php echo $row["quantity8"] ?>"></td>
-                                                            <td><input type="number" name="quantity9" min="0" max="10" class="scores" value="<?php echo $row["quantity9"] ?>"></td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>Group 4</td>
-                                                            <td><input type="number" name="quantity10" min="0" max="10" class="scores" value="<?php echo $row["quantity10"] ?>"></td>
-                                                            <td><input type="number" name="quantity11" min="0" max="10" class="scores" value="<?php echo $row["quantity11"] ?>"></td>
-                                                            <td><input type="number" name="quantity12" min="0" max="10" class="scores" value="<?php echo $row["quantity12"] ?>"></td>
-                                                        </tr>
-
-                                                    </table>
-                                                    <?php
+                                            </table>
+                                            <?php
                                                 }
-                                            }
+                                                }
                                             ?>
                                             <br>
                                             <input type="submit" value= "Update" name="submit" id="updatebutton">
                                             </form>
-                                            <div class="wasd domewasd">
-                                                <i class="fas fa-caret-up keycontrol up" data-command="0"></i>
-                                                <i class="fas fa-caret-right keycontrol right" data-command="4"></i>
-                                                <i class="fas fa-caret-down keycontrol down" data-command="2"></i>
-                                                <i class="fas fa-caret-left keycontrol left" data-command="6"></i>
-                                                <div class="middle keycontrol" data-command="1">STOP</div>
-                                            </div>
-                                            <script>
-                                                function control_camera(command) {
-                                                    //left = 6, right = 4, up = 0, down = 2 , stop = 1
-                                                    $("#dome-control").attr('src', 'http://foscam.serverict.nl/decoder_control.cgi?command=' + command);
-                                                }
-                                                $(document).ready(function () {
-                                                    $(".keycontrol").on('click', function () {
-                                                        var command = $(this).data('command');
-                                                        control_camera(command);
-                                                    });
-                                                });
-                                            </script>
-                                        </div>
                                         </div>
 
-                                        </div>
                                         </div>
                                         </div>
                                         </div>
